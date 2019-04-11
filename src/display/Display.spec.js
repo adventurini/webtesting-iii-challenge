5a8b221c7e44de33ed9a218da30c2209ad565e0d
Anthony Venturini <anthonydventurini@gmail.com>
2019-04-11 01:08:24 -0700

    Adds snapshot to Display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,29 @@
-// Test away!
+import React from 'react';
+import { 
+    
+    cleanup 
+} from 'react-testing-library';
+import renderer from 'react-test-renderer'; 
+import 'react-testing-library/cleanup-after-each'; 
+
+ import Display from './Display';
+
+ afterEach(() => {
+  cleanup();
+});
+
+ describe('<Display />', () => {
+  it.skip('matches snapshot', () => {
+    const tree = renderer.create(<Display />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+ describe('display open or close', () => {
+    it('displays open', () => {
+
+     })
+})
+
+
