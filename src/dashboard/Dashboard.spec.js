51f701440daa7db3b5306d0c6084cfc418413ac4
Anthony Venturini <anthonydventurini@gmail.com>
2019-04-11 01:03:19 -0700

    Adds snapshot to Dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,21 @@
-// Test away
+import React from 'react';
+import { 
+    cleanup 
+} from 'react-testing-library';
+import renderer from 'react-test-renderer'; 
+import 'react-testing-library/cleanup-after-each'; 
+
+ import Dashboard from './Dashboard';
+
+ afterEach(() => {
+  cleanup();
+});
+
+ describe('<Dashboard />', () => {
+  it.skip('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
